dfbe0f9e2a8ebf842c0775eb0bf8ca32e1e3aa4e
Kishan Jadav <kishan_jadav@hotmail.com>
2018-11-05 21:37:31 +0200

    Fix Trial: posting the data onto Firebase Db

--- app/src/main/java/mz/co/vida/HomeFragment.java
+++ app/src/main/java/mz/co/vida/HomeFragment.java
@@ -20,52 +20,68 @@ import java.util.List;
 import DAO.ConfiguracaoFirebase;
 import mehdi.sakout.fancybuttons.FancyButton;
 import mz.co.vida.entidades.Post;
+import android.widget.SearchView;
 
 public class HomeFragment extends Fragment {
 
+    // Components
     RecyclerView rv_posts;
-    PostAdapter postAdapter;
-    List<Post> postsList;
-    private DatabaseReference mdataRef;
-    private android.widget.SearchView searchView;
     private FancyButton mBtAnunciar;
+
+    // Vars
+    List<Post> postsList;
+
+    // Utils
+    PostAdapter postAdapter;
     ValueEventListener valueEventListener;
+    // Firebase Utils
+    private DatabaseReference mdataRef;
+    private SearchView searchView;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_home, container, false);
 
-         searchView = view.findViewById(R.id.pesquisar);
-         mBtAnunciar = view.findViewById(R.id.btn_anunciar);
-         rv_posts = view.findViewById(R.id.rv_posts);
-         rv_posts.setHasFixedSize(true);
-         mBtAnunciar.setOnClickListener(new View.OnClickListener() {
+        // Setup Utils
+        rv_posts = view.findViewById(R.id.rv_posts);
+        rv_posts.setHasFixedSize(true);
+
+        // Init Main
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Init Components
+        searchView  = view.findViewById(R.id.pesquisar);
+        mBtAnunciar = view.findViewById(R.id.btn_anunciar);
+        postsList = new ArrayList<Post>();
+
+        // Setup Components
+        mBtAnunciar.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 //Instancia o fragmentManager que é o responsável pela troca de Fragment
+                 // Instancia o fragmentManager que é o responsável pela troca de Fragment
                  FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                 //Instancia o fragment que você vai colocar na tela
+                 // Instancia o fragment que você vai colocar na tela
                  AnuncioFragment anuncioFragment = new AnuncioFragment();
-                 //Faz a transação, substituindo no frame da sua MainActivity que contem os fragmentos, o antigo pelo novo fragmento que você instanciou.
+                 // Faz a transação, substituindo no frame da sua MainActivity que contem os fragmentos, o antigo pelo novo fragmento que você instanciou.
                  fragmentManager.beginTransaction().replace(R.id.fl_framelayout, anuncioFragment).commit();
              }
-         });
+        });
 
 
+        // Setup RecyclerView
+        if (getActivity() != null){
+            rv_posts.setLayoutManager(new CustomLinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false)); // set the list layout style to vertical.
+            postAdapter = new PostAdapter(getActivity(), postsList);
+            rv_posts.setAdapter(postAdapter);
+        }
 
+        mdataRef = ConfiguracaoFirebase.getFirebase();
+        mdataRef.child("Usuario").orderByChild("disponibilidade").equalTo("Sim");
 
-        postsList = new ArrayList<Post>();
-         mdataRef = ConfiguracaoFirebase.getFirebase();
-         mdataRef.child("Usuario").orderByChild("disponibilidade").equalTo("Sim");
-
-         mdataRef.addValueEventListener(new ValueEventListener() {
+        mdataRef.addValueEventListener(new ValueEventListener() {
              @Override
              public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                  for (DataSnapshot info: dataSnapshot.getChildren()){
@@ -77,34 +93,47 @@ public class HomeFragment extends Fragment {
                      data.setEstado(post.getEstado());
                      data.setProvincia(post.getProvincia());
 
-                     System.out.println("Aqui "+info);
+                     // Nunca utilize System.out.println em código Java para Android. Instead, utilize `Log.`
+                     // @deprected System.out.println("Aqui "+info);
+                     Log.v("Log", "Aqui" + info);
                      postsList.add(data);
-                     Toast.makeText(getContext(), "Post", Toast.LENGTH_SHORT).show();
+
+                     /**
+                        Deixo isto para que vcs façam. É simples:
+                        Recomendo que alem de Adapter.notifyDataSetChanged(), utilizem o .notifyItemInserted(position)
+
+                        Arrangem uma forma de saber a posição do último item adicionado e acrescem 1, o que dará o position.
+                        Deste modo, vão ter uma forma mais fluida de adiconar elementos nesse recyclerview.
+
+                        Uma dica: já que tem o for() loop, há uma maneira de vocês incrementarem um número, ao se adicionar
+                        um elemento no recyclerView e assim terão a posição...
+
+
+                        // ou se desconseguirem podem deixar o notifyDataSetChanged() sem problemas.
+                      */
+                     postAdapter.notifyDataSetChanged();
+                     // Toast.makeText(getContext(), "Post", Toast.LENGTH_SHORT).show();
                  }
-                 if (getActivity() != null){
-                     rv_posts.setLayoutManager(new CustomLinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,false)); // set the list layout style to vertical.
-                     postAdapter = new PostAdapter(getActivity(), postsList);
-                     rv_posts.setAdapter(postAdapter);
-                 }
+
              }
              @Override
              public void onCancelled(@NonNull DatabaseError databaseError) {
-                 Toast.makeText(getContext(), "Bugeed", Toast.LENGTH_SHORT).show();
+                 // todo: remove on production...
+                 Toast.makeText(getContext(), "Bugged", Toast.LENGTH_SHORT).show();
              }
          });
 
         return view;
     }
+
+//     Post post = info.getValue(Post.class); // desnecessário
+
+
+
     public class CustomLinearLayoutManager extends LinearLayoutManager {
         public CustomLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
             super(context, orientation, reverseLayout);
         }
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        //mdataRef.addValueEventListener(valueEventListener);
-    }
 }
