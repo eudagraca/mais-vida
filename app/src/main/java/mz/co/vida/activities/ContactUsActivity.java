0caaa4717239dc35662119dfab69102fd3703d5a
Euclidio <euclidiodagraca@gmail.com>
2019-02-02 00:21:13 +0200

    Update, more info, about us. etc

---
+++ app/src/main/java/mz/co/vida/activities/ContactUsActivity.java
@@ -0,0 +1,29 @@
+package mz.co.vida.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import mz.co.vida.R;
+import mz.co.vida.fragments.MoreInfoFragment;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+
+        Button bt_back_more_info = (Button) findViewById(R.id.back_info);
+
+        bt_back_more_info.setOnClickListener(v -> onBackPressed());
+    }
+
+}
